229b9de3a55a56ea8d97139f5c1ecd44c2ac84f1
Paula <Paula@DESKTOP-CQI8V5P>
2024-01-08 23:21:14 +0100

    trianfin

--- src/Triangulo.java
+++ src/Triangulo.java
@@ -1,25 +1,21 @@
 import java.util.Scanner;
 
 public class Triangulo {
-
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Ingrese la longitud del primer lado del triángulo:");
-        int lado1 = scan.nextInt();
+        System.out.println("Ingrese la base del triángulo:");
+        int base = scanner.nextInt();
+        
+        System.out.println("Ingrese la altura del triángulo:");
+        int altura = scanner.nextInt();
 
-        System.out.println("Ingrese la longitud del segundo lado del triángulo:");
-        int lado2 = scan.nextInt();
+        double areaTriangulo = calcularAreaTriangulo(base, altura);
+        System.out.println("El área del triángulo es: " + areaTriangulo);
 
-        double area = calcularArea(lado1, lado2);
-        System.out.println("El área del triángulo es: " + area);
-
-        scan.close();
     }
 
-    public static double calcularArea(int lado1, int lado2) {
-        double area = 0.5 * lado1 * lado2;
-        int i = 0;
-        return area;
+    public static double calcularAreaTriangulo(int base, int altura) {
+        return (base * altura) / 2.0;
     }
 }
