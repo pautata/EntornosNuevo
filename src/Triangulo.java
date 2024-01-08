f868d3eb65d3e52aa5e79cb25335a49c57b1ed24
Paula <Paula@DESKTOP-CQI8V5P>
2024-01-09 00:31:17 +0100

    paulatriangulo

---
+++ src/Triangulo.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Triangulo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese la base del triángulo:");
+        int base = scanner.nextInt();
+        
+        System.out.println("Ingrese la altura del triángulo:");
+        int altura = scanner.nextInt();
+
+        double areaTriangulo = calcularAreaTriangulo(base, altura);
+        System.out.println("El área del triángulo es: " + areaTriangulo);
+
+        scanner.close();
+    }
+
+    public static double calcularAreaTriangulo(int base, int altura) {
+        return (base * altura) / 2.0;
+    }
+}
